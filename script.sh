0b884c8268202899d24338adff9ef66e187a0092
Rajnish Tripathi <82767514+RajnishXCode@users.noreply.github.com>
2025-01-22 15:16:08 +0530

    Update script.sh

--- script.sh
+++ script.sh
@@ -1,92 +1,113 @@
-# dfx identity new minter
+# Step 1: Create a new identity for the "minter" role.
+dfx identity new minter
+
+# Step 2: Switch to the "minter" identity.
 dfx identity use minter
-# export MINTER_ACCOUNT_ID=$(dfx ledger account-id)
-# dfx identity use office
-# export DEFAULT_ACCOUNT_ID=$(dfx ledger account-id)
-# dfx identity use minter
 
-# dfx deploy --specified-id ryjl3-tyaaa-aaaaa-aaaba-cai icp_ledger_canister --argument "
-#   (variant {
-#     Init = record {
-#       minting_account = \"$MINTER_ACCOUNT_ID\";
-#       initial_values = vec {
-#         record {
-#           \"$DEFAULT_ACCOUNT_ID\";
-#           record {
-#             e8s = 10_000_000_000 : nat64;
-#           };
-#         };
-#       };
-#       send_whitelist = vec {};
-#       transfer_fee = opt record {
-#         e8s = 10_000 : nat64;
-#       };
-#       token_symbol = opt \"LICP\";
-#       token_name = opt \"Local ICP\";
-#       feature_flags = opt record { icrc2 = true };
-#     }
-#   })
-# "
+# Step 3: Get the account ID for the "minter" identity and store it in the MINTER_ACCOUNT_ID variable.
+export MINTER_ACCOUNT_ID=$(dfx ledger account-id)
 
-
-
-
-# dfx deploy --specified-id mxzaz-hqaaa-aaaar-qaada-cai btc_ledger_canister --argument "
-#   (variant {
-#     Init = record {
-#       minting_account = \"$MINTER_ACCOUNT_ID\";
-#       initial_values = vec {
-#         record {
-#           \"$DEFAULT_ACCOUNT_ID\";
-#           record {
-#             e8s = 10_000_000_000 : nat64;
-#           };
-#         };
-#       };
-#       send_whitelist = vec {};
-#       transfer_fee = opt record {
-#         e8s = 10_000 : nat64;
-#       };
-#       token_symbol = opt \"LBTC\";
-#       token_name = opt \"Local BTC\";
-#       feature_flags = opt record { icrc2 = true };
-#     }
-#   })
-# "
-
-# dfx deploy --specified-id ss2fx-dyaaa-aaaar-qacoq-cai rj_ledger_canister --argument "
-# dfx deploy rj_ledger_canister --argument "
-#   (variant {
-#     Init = record {
-#       minting_account = \"$MINTER_ACCOUNT_ID\";
-#       initial_values = vec {
-#         record {
-#           \"$DEFAULT_ACCOUNT_ID\";
-#           record {
-#             e8s = 10_000_000_000 : nat64;
-#           };
-#         };
-#       };
-#       send_whitelist = vec {};
-#       transfer_fee = opt record {
-#         e8s = 10_000 : nat64;
-#       };
-#       token_symbol = opt \"RJC\";
-#       token_name = opt \"Local RJ-COIN\";
-#       feature_flags = opt record { icrc2 = true };
-#     }
-#   })
-# "
-dfx canister call icp_ledger_canister icrc1_transfer "(record { to = record { owner = principal \"zntu5-hhlwa-qgro2-sfxt7-c2pm5-7pa6y-brjps-x7r67-35slw-5bwjq-jqe\";}; amount = 2000_000_000;})"
-# dfx canister call btc_ledger_canister icrc1_transfer "(record { to = record { owner = principal \"sfwko-hd7us-gen5t-ssuci-vfjwf-afepb-a7p4y-guh5l-s5n2e-zuxvt-dae\";}; amount = 100_000_000;})"
-# dfx canister call eth_ledger_canister icrc1_transfer "(record { to = record { owner = principal \"sfwko-hd7us-gen5t-ssuci-vfjwf-afepb-a7p4y-guh5l-s5n2e-zuxvt-dae\";}; amount = 100_000_000;})"
-
+# Step 4: Switch to the "office" identity to fetch its account ID.
+# This identity will be used to receive initial tokens in the ledger canisters.
+# You can replace "office" with any other identity you want to use.
 dfx identity use office
-# # sfwko-hd7us-gen5t-ssuci-vfjwf-afepb-a7p4y-guh5l-s5n2e-zuxvt-dae
 
-# dfx canister call rj_ledger_canister icrc1_transfer "(record { to = record { owner = principal \"a7bvt-75bcc-77vhh-6ljps-3d2oz-5jolv-2zuzi-4r5zk-tf6vl-qhlsm-hae\";}; amount = 50000_000_000;})"
+# Step 5: Get the account ID for the "office" identity and store it in the DEFAULT_ACCOUNT_ID variable.
+export DEFAULT_ACCOUNT_ID=$(dfx ledger account-id)
 
+# Step 6: Switch back to the "minter" identity to perform further operations.
+dfx identity use minter
 
-# HL : 7rdme-3ekma-q43vc-gftmy-z4tbe-o33wx-dldtm-pnfpb-opnn3-esjbr-gae
-# CN : 
-# UR : lyruk-6lu53-qsd4t-7likt-kmkl2-6rvia-qbbsf-dzva6-chubv-leqgq-sae
+# --- Below 3 Ledger Canisters are created for ICP, BTC, and RJ tokens respectively. ---
+# -- Deploy the canisters with the your own configurations. --
+# -- Customize the canister IDs, token details, and amounts as needed for your use case. --
+
+# Step 7: Deploy the ICP Ledger Canister.
+# Replace `ryjl3-tyaaa-aaaaa-aaaba-cai` with your desired canister ID.
+dfx deploy --specified-id ryjl3-tyaaa-aaaaa-aaaba-cai icp_ledger_canister --argument "
+  (variant {
+    Init = record {
+      minting_account = \"$MINTER_ACCOUNT_ID\"; # Set the minter's account ID.
+      initial_values = vec {
+        record {
+          \"$DEFAULT_ACCOUNT_ID\"; # Assign initial tokens to the office account.
+          record {
+            e8s = 10_000_000_000 : nat64; # 10 ICP tokens in e8s (1 ICP = 100,000,000 e8s).
+          };
+        };
+      };
+      send_whitelist = vec {}; # Specify addresses allowed to receive transfers (empty = unrestricted).
+      transfer_fee = opt record {
+        e8s = 10_000 : nat64; # Transfer fee of 0.0001 ICP.
+      };
+      token_symbol = opt \"MY-ICP\"; # Symbol for the token.
+      token_name = opt \"Local ICP\"; # Name of the token.
+      feature_flags = opt record { icrc2 = true }; # Enable ICRC2 features.
+    }
+  })
+"
+
+# Step 8: Deploy the BTC Ledger Canister.
+# Replace `mxzaz-hqaaa-aaaar-qaada-cai` with your desired canister ID.
+dfx deploy --specified-id mxzaz-hqaaa-aaaar-qaada-cai btc_ledger_canister --argument "
+  (variant {
+    Init = record {
+      minting_account = \"$MINTER_ACCOUNT_ID\"; # Set the minter's account ID.
+      initial_values = vec {
+        record {
+          \"$DEFAULT_ACCOUNT_ID\"; # Assign initial tokens to the office account.
+          record {
+            e8s = 10_000_000_000 : nat64; # 10 BTC tokens in e8s.
+          };
+        };
+      };
+      send_whitelist = vec {}; # Specify addresses allowed to receive transfers (empty = unrestricted).
+      transfer_fee = opt record {
+        e8s = 10_000 : nat64; # Transfer fee of 0.0001 BTC.
+      };
+      token_symbol = opt \"LBTC\"; # Symbol for the token.
+      token_name = opt \"Local BTC\"; # Name of the token.
+      feature_flags = opt record { icrc2 = true }; # Enable ICRC2 features.
+    }
+  })
+"
+
+# Step 9: Deploy the RJ Ledger Canister.
+# Replace `ss2fx-dyaaa-aaaar-qacoq-cai` with your desired canister ID.
+dfx deploy --specified-id ss2fx-dyaaa-aaaar-qacoq-cai rj_ledger_canister --argument "
+  (variant {
+    Init = record {
+      minting_account = \"$MINTER_ACCOUNT_ID\"; # Set the minter's account ID.
+      initial_values = vec {
+        record {
+          \"$DEFAULT_ACCOUNT_ID\"; # Assign initial tokens to the office account.
+          record {
+            e8s = 10_000_000_000 : nat64; # 10 RJ tokens in e8s.
+          };
+        };
+      };
+      send_whitelist = vec {}; # Specify addresses allowed to receive transfers (empty = unrestricted).
+      transfer_fee = opt record {
+        e8s = 10_000 : nat64; # Transfer fee of 0.0001 RJ.
+      };
+      token_symbol = opt \"RJC\"; # Symbol for the token.
+      token_name = opt \"Local RJ-COIN\"; # Name of the token.
+      feature_flags = opt record { icrc2 = true }; # Enable ICRC2 features.
+    }
+  })
+"
+
+# Step 10: Perform an ICRC1 transfer from the ICP Ledger Canister.
+# Replace the `to` owner principal ID with the target account's principal.
+dfx canister call icp_ledger_canister icrc1_transfer "(record { 
+  to = record { owner = principal \"oet34-p2e2h-wp2yv-jy7kz-kgscj-bj5j2-a72fh-ivb26-at2ss-htoiu-vae\"; }; 
+  amount = 2_000_000_000; # Transfer 2 ICP tokens in e8s.
+})"
+
+# Step 11: Switch back to the "office" identity.
+dfx identity use office
+
+# Notes:
+# - Replace `principal` IDs with actual target principals for each transaction.
+# - Customize canister IDs, token details, and amounts as needed for your use case.
+# - Always ensure your ledger canisters are correctly configured before deployment.
